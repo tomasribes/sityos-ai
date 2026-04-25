deed09b49478401e27322eec992a1458684c7ca6
Tomas Ribes <tomas.ribes@ogilvy.com>
2026-04-26 00:08:02 +0200

    build: manual (master) [2026-04-26T00:07:20Z]

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '9c85cdab3dbf1e8a';
+  public const string VERSIONS_HASH = '78cc1a0a577f5731';
 
 }
